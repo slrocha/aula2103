2c775edde2fa55778720e01994671933258d300a
slrocha <slrocha17@gmail.com>
2024-03-21 21:43:04 -0300

    Aula 2103 - Questão 02

--- src/Colaborador.java
+++ src/Colaborador.java
@@ -1,2 +1,53 @@
 public class Colaborador {
+
+    private String nome;
+    private String sobrenome;
+    private double salario;
+
+    public void  Colaborador(String nome, String sobrenome, double salario){
+        this.nome = nome;
+        this.sobrenome = sobrenome;
+        this.salario = salario;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getSobrenome() {
+        return sobrenome;
+    }
+
+    public void setSobrenome(String sobrenome) {
+        this.sobrenome = sobrenome;
+    }
+
+    public double getSalario() {
+        return salario;
+    }
+
+    public void setSalario(double salario) {
+        if (salario < 0) {
+            this.salario = 0.0;
+        } else {
+            this.salario = salario;
+        }
+    }
+    @Override
+    public String toString() {
+        return "Colaborador{" +
+                "nome='" + nome + '\'' +
+                ", sobrenome='" + sobrenome + '\'' +
+                ", salario=" + salario +
+                '}';
+    }
+
+    public double aumento(double salario, int aumento){
+        double result = salario*((double) aumento /100);
+        return this.salario + result;
+    }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,40 @@
 public class Main {
 
     public static void main(String[] args) {
-        Suprimentos suprimentos = new Suprimentos();
+        /*Suprimentos suprimentos = new Suprimentos();
         suprimentos.setNumero(1);
         suprimentos.setDescricao("Computador de Mesa");
-        suprimentos.setPreco(2500);
+        suprimentos.setPreco(-1);
         suprimentos.setQuantidade(2);
 
 
         System.out.println("Informações do item: " + suprimentos.toString());
         System.out.println("Fatura do item " + suprimentos.getDescricao() + " = "
-                + suprimentos.getInvoiceAmount(suprimentos.getQuantidade(), suprimentos.getPreco()));
+                + suprimentos.getInvoiceAmount(suprimentos.getQuantidade(), suprimentos.getPreco()));*/
+
+        Colaborador colaborador = new Colaborador();
+        Colaborador colaborador1 = new Colaborador();
+
+        colaborador1.setNome("Maria");
+        colaborador1.setSobrenome("Aparecida");
+        colaborador1.setSalario(1500);
+
+        colaborador.setNome("Stephanie");
+        colaborador.setSobrenome("Rocha");
+        colaborador.setSalario(4500);
+
+        System.out.println("Salario anual do Colaborador" +colaborador1.getNome() +" antes do aumento: "
+                + colaborador1.getSalario());
+        System.out.println("Salario anual do Colaborador" +colaborador.getNome() +" antes do aumento: "
+                + colaborador.getSalario());
+
+        colaborador1.setSalario(colaborador1.aumento(colaborador1.getSalario(), 10));
+        colaborador.setSalario(colaborador.aumento(colaborador.getSalario(), 10));
+
+        System.out.println("Salario anual do Colaborador " +colaborador1.getNome() +" após do aumento: "
+                + colaborador1.getSalario());
+        System.out.println("Salario anual do Colaborador " +colaborador.getNome() +" após do aumento: "
+                + colaborador.getSalario());
+
     }
 }
